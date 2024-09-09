0db33bb71ce6e19a3a4e1d2c6143099193852103
jorgeallende <jorgeallende333@gmail.com>
2024-09-09 10:55:20 -0300

    ADD: Delete treinamento

--- src/pages/treinamentos/treinamentos.tsx
+++ src/pages/treinamentos/treinamentos.tsx
@@ -2,16 +2,25 @@ import EditIcon from "@/components/icons/EditIcon";
 import FourRectanglesIcon from "@/components/icons/FourRectanglesIcon";
 import TrashIcon from "@/components/icons/TrashIcon";
 import DataTableBase from "@/components/partials/datatable";
+import DeleteModal from "@/components/partials/deleteModal";
 import { Button } from "@/components/ui/button";
+import { KeycloakContext } from "@/services/useAuth";
 import { useMeta } from "@/services/useMeta";
+import { API_URL_ADMIN } from "@/utils/consts";
 import { useGetTreinamentos } from "@/utils/queries";
-import { GetTreinamentosTYPE } from "@/utils/types";
+import { GetTreinamentosTYPE, ModalActions } from "@/utils/types";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { useMutation } from "react-query";
 import { useNavigate } from "react-router-dom";
 
 const Treinamentos = () => {
   const navigate = useNavigate();
-
+  const { keycloak } = useContext(KeycloakContext);
+  const token = keycloak?.token;
   const { perPage, page, searchBy, debouncedSearch } = useMeta();
+  const [modal, setModal] = useState<ModalActions>("");
+  const [idToDelete, setIdToDelete] = useState<string>("");
 
   const treinamentos = useGetTreinamentos({
     per_page: perPage,
@@ -21,11 +30,6 @@ const Treinamentos = () => {
   });
 
   const columns = [
-    // {
-    //     name: <b>Valor Auxiliar</b>,
-    //     selector: (row: any) => row.valor_auxiliar.descricao,
-    //     sortable: true,
-    // },
     {
       name: <>Titulo</>,
       selector: (row: GetTreinamentosTYPE) => row.titulo ?? "-",
@@ -42,9 +46,8 @@ const Treinamentos = () => {
             variant="destructive"
             size="sm"
             onClick={() => {
-              if (row) {
-                console.log(row);
-              }
+              setIdToDelete(row.id);
+              setModal("delete");
             }}
           >
             <TrashIcon />
@@ -72,6 +75,26 @@ const Treinamentos = () => {
     </div>
   );
 
+  const deleteData = useMutation(
+    async (id: string) => {
+      const { data } = await axios.delete(
+        `${API_URL_ADMIN}/treinamentos/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return data;
+    },
+    {
+      onSuccess: () => {
+        setModal("");
+        treinamentos.refetch();
+      },
+    }
+  );
+
   return (
     <div>
       <div>
@@ -80,6 +103,14 @@ const Treinamentos = () => {
             Ocorreu um erro ao carregar <strong>treinamentos</strong>
           </div>
         )}
+        <DeleteModal
+          open={modal}
+          onSubmit={() => deleteData.mutate(idToDelete ?? "")}
+          setOpen={setModal}
+          isSuccess={deleteData.isSuccess}
+          isError={deleteData.isError}
+          isLoading={deleteData.isLoading}
+        />
         <div className="px-4">
           <DataTableBase<GetTreinamentosTYPE>
             subHeader
