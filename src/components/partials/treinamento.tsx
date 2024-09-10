812dc56685d4ca1221b5c70a6a5089131fbe9e87
jorgeallende <jorgeallende333@gmail.com>
2024-09-10 09:56:18 -0300

    ADD: Select em filtro de busca

--- src/components/partials/treinamento.tsx
+++ src/components/partials/treinamento.tsx
@@ -11,74 +11,71 @@ const Treinamento = ({
   nome_do_autor,
   data_publicacao,
   resumo,
-  id
-
-}:{
-  tags: GetTagsTYPE[]
-  capa: {url: string}
-  titulo: string
-  nome_do_autor: string
-  data_publicacao: string
-  resumo: string
-  id: string
+  id,
+}: {
+  tags: GetTagsTYPE[];
+  capa: { url: string };
+  titulo: string;
+  nome_do_autor: string;
+  data_publicacao: string;
+  resumo: string;
+  id: string;
 }) => {
   return (
-    
-  <div className="grid grid-rows-2 grid-flow-col gap-4">
+    <div className="grid grid-rows-2 grid-flow-col gap-4">
+      <div className="row-span-3 ...">
+        <img
+          src={`${BASE_URL}${capa.url}`}
+          className="rounded-md object-cover w-full aspect-video"
+          alt="Ruby"
+        />
+      </div>
+      <div className="col-span-2 ...">
+        <div className="flex justify-between gap-2 mt-2">
+          <h1 className="font-bold text-green-700 text-xs">
+            {tags.map((tag) => tag.titulo).join(", ")}
+          </h1>
 
-  <div className="row-span-3 ...">
+          <div className="flex gap-4">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded-full ">
+              {" "}
+              Button{" "}
+            </button>
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-10 rounded-full">
+              {" "}
+              Button{" "}
+            </button>
+          </div>
+        </div>
 
-  <img
-              src={`${BASE_URL}${capa.url}`}
-              className="rounded-md object-cover w-full aspect-video"
-              alt="Ruby"
-            />
+        <div className="flex justify gap-4 mt-4">
+          <h1 className="font-bold text-pmmBlue">{titulo}</h1>
+        </div>
 
-  </div>
-  <div className="col-span-2 ..."> 
-    <div className="flex justify-between gap-2 mt-2">
-    <h1 className="font-bold text-green-700 text-xs">{
-      tags.map((tag) => tag.titulo).join(", ")
-      }</h1>
+        <div className="flex justify gap-4 mt-4">
+          <img
+            className="rounded-full object-cover w-10 h-10 aspect-video"
+            src={Ruby}
+          />
+          <p className="text-pmmGray">{nome_do_autor}</p>
+          <p className="text-pmmGray">
+            {dayjs(data_publicacao).format("DD/MM/YYYY")}
+          </p>
+        </div>
+      </div>
 
-    <div className="flex gap-4">
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-10 rounded-full "> Button </button>                           
-    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-10 rounded-full"> Button </button>
-                  
+      <div className="row-span-2 col-span-2 ...">
+        <span className="text-sm text-pmmBlue ">{resumo}</span>
+        <div className="flex justify-end gap-4 mt-4">
+          <a
+            href={`/treinamento/${id}`}
+            className="text-xs hover:underline w-max text-pmmBlue "
+          >
+            Visualizar artigo &gt;
+          </a>
+        </div>
+      </div>
     </div>
-
-  </div>
-
-  <div className="flex justify gap-4 mt-4">
-      <h1 className="font-bold text-pmmBlue">T{titulo}</h1>
-    
-  </div>
-  
-  <div className="flex justify gap-4 mt-4"> 
-      
-
-      <img className="rounded-full object-cover w-10 h-10 aspect-video" 
-          src={Ruby}
-      />
-      <p className="text-pmmGray">{nome_do_autor}</p>
-      <p className="text-pmmGray">{dayjs(data_publicacao).format("DD/MM/YYYY")}</p>                        
-           
-  </div>   
-   
-  </div>
-
-  <div className="row-span-2 col-span-2 ...">             
-           
-                <span className="text-sm text-pmmBlue ">
-                  {resumo}
-                </span>
-  <div className="flex justify-end gap-4 mt-4">
-                <a href={`/treinamento/${id}`} className="text-xs hover:underline w-max text-pmmBlue ">
-                  Visualizar artigo &gt;
-                </a></div>
-    </div>
-  </div>
- 
   );
-}
+};
 export default Treinamento;
--- src/pages/busca.tsx
+++ src/pages/busca.tsx
@@ -1,81 +1,130 @@
-import React, { useEffect, useState } from "react";
-import Aula from "../components/partials/treinamento";
-import { Link, useSearchParams } from "react-router-dom";
-import axios, { all } from "axios";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
 import { useQuery } from "react-query";
 import { API_URL } from "@/utils/consts";
-import { GetTagsTYPE, GetTreinamentosTYPE, GetCategoriasTYPE, TreinamentosType } from '../utils/types';
+import { GetTreinamentosTYPE, TreinamentosType } from "../utils/types";
 import Treinamento from "../components/partials/treinamento";
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@radix-ui/react-select";
-import { AiFillTablet } from "react-icons/ai";
-import PickList from "@/components/partials/pickList";
-
-
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useGetCategorias, useGetTags } from "@/utils/queries";
 
 const Busca = () => {
-  const [selectedTag, setSelectedTag] = useState('');
-  const [selectedCategoria, setSelectedCategoria] = useState('');
+  const [selectedTag, setSelectedTag] = useState<string | undefined>();
+  const [selectedCategoria, setSelectedCategoria] = useState<
+    string | undefined
+  >();
 
-  
-  
-  const [search, setSearch] = useSearchParams()
+  const [search, setSearch] = useSearchParams();
+  const categorias = useGetCategorias({ per_page: "all" });
+  const tags = useGetTags({ per_page: "all" });
 
-  const busca= search.get('search')
+  const busca = search.get("search");
 
   const getData = useQuery({
-      queryKey: ['treinamentos, tags, categorias'],
-      queryFn: async () => {
-          const { data } = await axios.get<GetTreinamentosTYPE[]>(
-              `${API_URL}/treinamentos`,
-              {
-                  headers: {
-                      Authorization: `Bearer ${localStorage.getItem('token')}`,
-                  },
-                  params:{
-                      search: search.get('search'),
-                      searchBy: 'all'
-                  }
-              }
-            )
-            return data
+    queryKey: ["treinamentos, tags, categorias"],
+    queryFn: async () => {
+      const { data } = await axios.get<GetTreinamentosTYPE[]>(
+        `${API_URL}/treinamentos`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
-        })
-        
+          params: {
+            search: search.get("search"),
+            categoria_id: search.get("categoria_id"),
+            tag_id: search.get("tag_id"),
+            searchBy: "all",
+          },
+        }
+      );
+      return data;
+    },
+  });
+
   useEffect(() => {
-      getData.refetch()
-  }, [search.get('search')])
- 
+    getData.refetch();
+  }, [search.get("search")]);
+
+  const submitFiltrar = () => {
+    getData.refetch();
+  };
+
   return (
     <div>
       <div className="mt-4 mb-8">
-        <div className="w-8 h-1 bg-green-600 rounded-full" mb-8></div>  
-        <span className="uppercase text-pmmBlue font-bold text-xs ">TREINAMENTO</span>
+        <div className="w-8 h-1 bg-green-600 rounded-full" mb-8></div>
+        <span className="uppercase text-pmmBlue font-bold text-xs ">
+          TREINAMENTO
+        </span>
         <div className="flex flex-2 justify-between  gap-2">
-          <h1 className="text-pmmBlue text-4xl font-bold">Resultados para "{busca}"</h1>
-          <div className="flex w-41 justify-between gap-2">
-            <form>
-            <Select>
-              <SelectTrigger className="border border-gray-300 rounded-md shadow-sm focus:ring-pmmBlue focus:border-pmmBlue sm:text-sm">
-                <SelectValue>
-                  <span className="block truncate">Categoria</span>
-                </SelectValue>
-                <SelectTrigger>
-                  <AiFillTablet className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                </SelectTrigger>
+          <h1 className="text-pmmBlue text-4xl font-bold">
+            {busca ? `Resultados para: ${busca}` : "Resultados"}
+          </h1>
+          <div className="flex justify-between gap-2">
+            <Select
+              value={selectedCategoria}
+              onValueChange={setSelectedCategoria}
+            >
+              <SelectTrigger className="w-[300px]">
+                <SelectValue placeholder="Filtrar por categoria" />
               </SelectTrigger>
-                </Select>
+              <SelectContent>
+                <SelectGroup>
+                  {categorias.data?.categorias.map((categoria) => (
+                    <SelectItem
+                      key={categoria.id}
+                      value={categoria.id}
+                      onClick={() => {
+                        setSearch({
+                          search: busca,
+                          categoria_id: categoria.id,
+                        });
+                        setSelectedCategoria(categoria.id);
+                      }}
+                    >
+                      {categoria.titulo}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
 
-            </form>
+            <Select value={selectedTag} onValueChange={setSelectedTag}>
+              <SelectTrigger className="w-[300px]">
+                <SelectValue placeholder="Filtrar por tag" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  {tags.data?.tags.map((tag) => (
+                    <SelectItem
+                      key={tag.id}
+                      value={tag.id}
+                      onClick={() => {
+                        setSearch("tag", tag.id);
+                        setSelectedTag(tag.id);
+                      }}
+                    >
+                      {tag.titulo}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
 
+            <button onClick={submitFiltrar}>Filtrar</button>
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-10 ms-8">
+      <div className="flex flex-col gap-10">
         {getData.data?.treinamentos.map((treinamento: TreinamentosType) => {
-        
-          return (        
-          
+          return (
             <Treinamento
               key={treinamento.id}
               tags={treinamento.tags}
@@ -84,117 +133,110 @@ const Busca = () => {
               nome_do_autor={treinamento.nome_do_autor}
               data_publicacao={treinamento.data_publicacao}
               resumo={treinamento.resumo}
-              id={treinamento.id}        
-            />   
+              id={treinamento.id}
+            />
           );
         })}
-        
 
-<div className="flex flex-col gap-10 ms-8">
-        {getData.data?.treinamentos.map((treinamento: TreinamentosType) => {
-          
-          
-          const a = treinamento.tags.map((tag) => tag.titulo).join(", ");
-          const allTitles = new Set([a]);
-          const uniqueTitles = Array.from(allTitles);
-      
-          console.log(uniqueTitles); 
-          
-        
-        })}
+        <div className="flex flex-col gap-10 ms-8">
+          {getData.data?.treinamentos.map((treinamento: TreinamentosType) => {
+            const a = treinamento.tags.map((tag) => tag.titulo).join(", ");
+            const allTitles = new Set([a]);
+            const uniqueTitles = Array.from(allTitles);
 
+            console.log(uniqueTitles);
+          })}
 
+          <div className="relative mt-11"></div>
 
-      <div className="relative mt-11"></div>
-      
-
-      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-        <div className="flex flex-1 justify-between sm:hidden">
-          <a
-            href="#"
-            className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-            >
-            Anterior
-          </a>
-          <a
-            href="#"
-            className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-            >
-            Próximo
-          </a>
-        </div>
-        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-          <div>
-            <p className="text-sm text-gray-700">
-              Mostrando <span className="font-medium">1</span> de <span className="font-medium">10</span> de{' '}
-              <span className="font-medium">97</span> resultados
-            </p>
-          </div>
-          <div>
-            <nav aria-label="Pagination" className="isolate inline-flex -space-x-px rounded-md shadow-sm">
+          <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+            <div className="flex flex-1 justify-between sm:hidden">
               <a
                 href="#"
-                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                >
-                <span className="sr-only">Previous</span>
-              </a>
-              {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-              <a
-                href="#"
-                aria-current="page"
-                className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                1
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                >
-                2
-              </a>
-              <a
-                href="#"
-                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-                >
-                3
-              </a>
-              <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-                
-                ...
-              </span>
-              <a
-                href="#"
-                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
               >
-                8
+                Anterior
               </a>
               <a
                 href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
               >
-                9
+                Próximo
               </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                10
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                <span className="sr-only">Próximo</span>
-              </a>
-            </nav>
+            </div>
+            <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+              <div>
+                <p className="text-sm text-gray-700">
+                  Mostrando <span className="font-medium">1</span> de{" "}
+                  <span className="font-medium">10</span> de{" "}
+                  <span className="font-medium">97</span> resultados
+                </p>
+              </div>
+              <div>
+                <nav
+                  aria-label="Pagination"
+                  className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+                >
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  >
+                    <span className="sr-only">Previous</span>
+                  </a>
+                  {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
+                  <a
+                    href="#"
+                    aria-current="page"
+                    className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    1
+                  </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  >
+                    2
+                  </a>
+                  <a
+                    href="#"
+                    className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                  >
+                    3
+                  </a>
+                  <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
+                    ...
+                  </span>
+                  <a
+                    href="#"
+                    className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                  >
+                    8
+                  </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  >
+                    9
+                  </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  >
+                    10
+                  </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  >
+                    <span className="sr-only">Próximo</span>
+                  </a>
+                </nav>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
-   
-
   );
-}
+};
 export default Busca;
