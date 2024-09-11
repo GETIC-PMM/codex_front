04add853314f4545f033a69ab1ba2252470ede29
jorgeallende <jorgeallende333@gmail.com>
2024-09-11 14:01:55 -0300

    FIX: Centralizando input de buscar

--- src/components/layout/siteLayout/partials/header.tsx
+++ src/components/layout/siteLayout/partials/header.tsx
@@ -6,26 +6,22 @@ import { useState } from "react";
 import { API_URL } from "@/utils/consts";
 import { useNavigate } from "react-router-dom";
 
-
-
 const Header = () => {
-
   const [search, setSearch] = useState("");
   const [results, setResults] = useState([]);
   const navigate = useNavigate();
   const handleSearch = async () => {
     if (search.trim() === "") return;
     try {
-
       const response = await axios.get(`${API_URL}/treinamentos`);
-      setResults(response.data);     
+      setResults(response.data);
     } catch (error) {
       console.error("Erro ao buscar treinamentos", error);
     }
-  }
+  };
 
   return (
-    <div className="flex justify-between w-full py-5 mb-10">
+    <div className="flex justify-between w-full py-5 mb-10 items-center">
       <div className="flex gap-10">
         <a href="/">
           <img src={DiretiLogo} width={250} alt="Direti Logo" />
@@ -33,22 +29,24 @@ const Header = () => {
 
         {/* TODO: Colocar icone na barra de busca */}
         <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          navigate("/busca?search=" + search);}}
+          onSubmit={(e) => {
+            e.preventDefault();
+            navigate("/busca?search=" + search);
+          }}
+          className="flex items-center"
         >
-        <input
-          type="text"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}     
-          className="border-b border-pmmGray placeholder:text-pmmGray placeholder:text-xs focus:outline-none"
-          placeholder="Pesquisar"
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="border-b border-pmmGray placeholder:text-pmmGray placeholder:text-xs focus:outline-none"
+            placeholder="Pesquisar"
           />
-          </form>
+        </form>
       </div>
       <div className="flex gap-16 items-center text-pmmGray">
         <a href="/login">Sobre a DIRETI</a>
-        <a href="/register">Treinamentos</a>
+        <a href="/busca?search=">Treinamentos</a>
         <a
           href="/painel"
           className={buttonVariants() + "bg-pmmBlue font-semibold text-sm"}
